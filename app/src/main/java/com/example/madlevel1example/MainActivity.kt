0463fd7949ddcee9dad6875567a54e00f9a7eca4
Robin <dtr.robin09@gmail.com>
2020-09-06 01:54:53 +0200

    Example done

--- app/src/main/java/com/example/madlevel1example/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1example/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.madlevel1example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnConfirm.setOnClickListener {
+            checkAnswer()
+        }
     }
+
+    private fun checkAnswer() {
+        val answer = binding.etAnswer.text.toString()
+
+        when (answer) {
+            getString(R.string.giraffe) -> showToast(getString(R.string.correct))
+            else ->  showToast(getString(R.string.incorrect))
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
 }
